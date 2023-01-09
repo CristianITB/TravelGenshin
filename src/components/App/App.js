c9417ea3f0c697a6ca5199bfac9787e0faefb9b7
Cristian <cristian,jimenez.7e5@itb.cat>
2023-01-09 11:45:10 +0100

    creating some structure II

---
+++ src/components/App/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p>Good morning</p>
+    </div>
+  );
+}
+
+export default App;
