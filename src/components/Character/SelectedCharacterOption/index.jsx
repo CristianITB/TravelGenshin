5a3f5bdb242e5ce89bffd1e6a706aba46d77c93c
Cristian <cristian,jimenez.7e5@itb.cat>
2023-01-16 13:37:33 +0100

    sergi carrying

--- src/components/Character/SelectedCharacterOption/index.jsx
+++ src/components/Character/SelectedCharacterOption/index.jsx
@@ -1,7 +1,8 @@
-import { Frame, Title, UnorderedList, LiElement } from '../../Header/styles'
-import React, { useState, useEffect} from 'react'
-import axios from 'axios'
-import { SingleElementDisplayer } from '../SingleElementDisplayer'
+// import { Frame, Title, UnorderedList, LiElement } from '../../Header/styles'
+// import React, { useState, useEffect} from 'react'
+// import axios from 'axios'
+// import { SingleElementDisplayer } from '../SingleElementDisplayer'
+import { useApiData } from '../../../helpers/axios.helper'
 
 // export const SelectedCharacterOption = ({element}) => {
 //   return(
@@ -16,23 +17,32 @@ import { SingleElementDisplayer } from '../SingleElementDisplayer'
 
 
 export const SelectedCharacterOption = (props) => {
-  const [data, setData] = useState(null);
+  // const [data, setData] = useState(null);
 
-  useEffect(() => {
-      axios.get("https://api.genshin.dev/characters", {
-          params: {
-              param1: props.names
-          }
-      }).then(response => {
-          setData(response.data);
-      });
-  }, [props]);
+  const charactersDataFromApi = useApiData()
+  console.log(charactersDataFromApi[0])
+  // const [charactersDataListState, setCharactersDataListState] = useState([])
+  // const modifyCharactersList = () => {
+  //   setCharactersDataListState([...charactersDataListState, charactersDataFromApi.results[2]])
+  // }
+
+  // useEffect(() => {
+  //     axios.get("https://api.genshin.dev/characters", {
+  //         params: {
+  //             param1: props.names
+  //         }
+  //     }).then(response => {
+  //         setData(response.data);
+  //     });
+  // }, [props]);
 
   return (
       <div>
+        {/* <button onClick={modifyCharactersList}>Add {props.element} character</button>
+        <button onClick={() => console.log('clicked two')}>Show all {props.element} characters</button>
           {data && (
               <SingleElementDisplayer {...data} />
-          )}
+          )} */}
       </div>
   );
 }
--- src/helpers/axios.helper.js
+++ src/helpers/axios.helper.js
@@ -1,16 +1,52 @@
 import axios from 'axios'
 import React from 'react'
 
-export function ApiHelper (baseURL) {
+export function useApiData (baseURL) {
   const [post, setPost] = React.useState(null)
 
   React.useEffect(() => {
-    axios.get(baseURL).then((response) => {
-      setPost(response.data)
-    })
+    const fetchData = async () => {
+      const manolo = await GetAllCharactersData()
+      setPost(manolo)
+    }
+    fetchData()
   }, [baseURL])
-
   return post
 }
 
-export default ApiHelper
+async function getCharactersNames(){
+
+   const pepe = await axios.get('https://api.genshin.dev/characters').then((response) => {return response})
+
+  return pepe.data
+}
+
+async function GetOneCharacterInfo(charactersNameList){
+
+  const datachar = await axios.all(charactersNameList.map( async (characterName) => {
+    const prueba = await axios.get('https://api.genshin.dev/characters/'+characterName)
+    return prueba.data
+  }))
+  return datachar
+}
+
+async function GetAllCharactersData(){
+  // let allCharactersData = []
+  const charactersNames = await getCharactersNames()
+  const datachar = GetOneCharacterInfo(charactersNames)
+  return datachar
+  // if(charactersNames == null){
+
+  // }
+
+  // console.log(charactersNames)
+  // for(let i=0; i<charactersNames.length; i++){
+  //   allCharactersData.push(GetOneCharacterInfo(charactersNames[i]))
+  // }
+
+  // return allCharactersData
+}
+
+function getIt(){
+  
+}
