cd56832d3ec9e7012651db863561db008cfab812
Cristian <cristian,jimenez.7e5@itb.cat>
2023-01-20 10:31:44 +0100

    updated

--- src/components/OST/OSTOptions/index.jsx
+++ src/components/OST/OSTOptions/index.jsx
@@ -4,16 +4,16 @@ export const OSTOptions = (props) => {
   const handleClick = (event) => {
     props.onMenuClick(event.target.innerText)
   }
-  return(
+  return (
     <Frame>
-        <TitleComponent>OST's</TitleComponent>
-        <UnorderedList>
-          <AddCountry onClick={handleClick}>Mondstadt</AddCountry>
-          <AddCountry onClick={handleClick}>Liyue</AddCountry>
-          <AddCountry onClick={handleClick}>Inazuma</AddCountry>
-          <AddCountry onClick={handleClick}>Sumeru</AddCountry>
-        </UnorderedList>
-        </Frame>
+      <TitleComponent>OST's</TitleComponent>
+      <UnorderedList>
+        <AddCountry onClick={handleClick}>Mondstadt</AddCountry>
+        <AddCountry onClick={handleClick}>Liyue</AddCountry>
+        <AddCountry onClick={handleClick}>Inazuma</AddCountry>
+        <AddCountry onClick={handleClick}>Sumeru</AddCountry>
+      </UnorderedList>
+    </Frame>
   )
 }
 
