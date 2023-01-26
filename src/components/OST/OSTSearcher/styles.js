9d656bd594e619b560c2b6ce91cb1dcf299b7a88
Zincraw <xaviergs99@gmail.com>
2023-01-26 12:59:43 +0100

    some paddings added to the token

--- src/components/OST/OSTSearcher/styles.js
+++ src/components/OST/OSTSearcher/styles.js
@@ -1,18 +1,20 @@
 import styled from '@emotion/styled'
 
 export const InputBox = styled.input`
-    width: 60vw;
+    width: 40vw;
     margin: auto;
     display: block;
     padding: 1vw;
-    border: 2px solid #000000;
+    border: 0.5px solid #000000;
 `
 export const SubmitButton = styled.button`
+    width: 20vw;
+    font-size: 1vw;
     margin: auto;
-    padding: 1vw;
+    padding: 0.5vw;
     display: block;
     margin-top: 1vw;
-    border-radius: 20px;
+    border-radius: 1vw;
     background-color: black;
     color: white;
 `
